528ab193d7f50ef2098c2436d3e7e9b45e6d9b4e
chantla <44217488+ycnj@users.noreply.github.com>
2018-12-03 10:32:58 +0900

    Update Config.java

--- BatSearch2/src/test/search/action/Config.java
+++ BatSearch2/src/test/search/action/Config.java
@@ -1,7 +1,7 @@
 package test.search.action;
 
 public class Config {
-	private String apiKey="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJhODk3OGUyMC1kMDI5LTAxMzYtYzU2ZC0yOWVkYjMzMTRhZGMiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTQyODUyNjA4LCJwdWIiOiJibHVlaG9sZSIsInRpdGxlIjoicHViZyIsImFwcCI6Ii1hMWI2ZWJlZS1hYzI0LTQwMzEtYTQ0Zi1jNWM3MTQ3Y2VkZWUifQ.ihJ45dIUBmpWKXzu2Jsm2wdRAr8pSWvh6Wsc_7e-gFg";
+	private String apiKey="";
 
 	public String getApiKey() {
 		return apiKey;
